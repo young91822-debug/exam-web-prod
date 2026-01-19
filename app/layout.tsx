d0956e89efa768b6e248d79616d1ded155676903
young91822-debug <young91822@gmail.com>
2026-01-19 16:42:04 +0900

    fix: restore tailwind css on prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "./globals.css";
+import React from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="min-h-[100dvh]">{children}</body>
     </html>
   );
 }
